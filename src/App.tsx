eef83aeb17684bdac48713784c88a50fa82a89b8
Mauricio Blum <mauricio.blum@itsector.pt>
2020-07-24 12:25:16 +0100

    current app example

---
+++ src/App.tsx
@@ -0,0 +1,81 @@
+/* eslint-disable global-require */
+import { StatusBar } from 'expo-status-bar';
+import { useFonts } from 'expo-font';
+import { AppLoading } from 'expo';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import { Card } from 'react-native-superdda-iupay';
+
+import studentImg from './assets/images/student.png';
+
+export default function App(): JSX.Element {
+  const [fontsLoaded] = useFonts({
+    'NunitoSans-Bold': require('./assets/fonts/NunitoSans-Bold.ttf'),
+    'NunitoSans-ExtraBold': require('./assets/fonts/NunitoSans-ExtraBold.ttf'),
+    'NunitoSans-Light': require('./assets/fonts/NunitoSans-Light.ttf'),
+    'NunitoSans-Regular': require('./assets/fonts/NunitoSans-Regular.ttf'),
+    'NunitoSans-SemiBold': require('./assets/fonts/NunitoSans-Bold.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <View style={styles.app}>
+      <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Card
+          type="netflix"
+          dueDate="Vencendo hoje, 23 JUL"
+          isDue
+          cnpj="99.999.0001/99"
+          text="This is a sample text"
+          containerStyle={{ marginBottom: 20 }}
+          value={49.9}
+        />
+
+        <Card
+          type="default"
+          cardTitle="COLÉGIO XZ"
+          textColor="#727272"
+          logo={{ uri: studentImg }}
+          dueDate="29 JUL"
+          cnpj="99.999.0001/99"
+          text="Débito automático no dia do vencimento"
+          containerStyle={{ marginBottom: 20 }}
+          value={300}
+          barColor="#125598"
+        />
+
+        <Card
+          type="lightBill"
+          dueDate="25 JUL"
+          cnpj="99.999.0001/99"
+          isPaid
+          text="Bandeira Amarela"
+          lightBillFlagStatus="yellow"
+          value={121.45}
+        />
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  app: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#f78c49',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 15,
+    minWidth: 414,
+  },
+});
